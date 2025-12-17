d3b48a49278e96fcc7c6fd4bc23f759b322eb200
Naznin Basheer <naznin0721@gmail.com>
2025-12-17 15:18:48 +0530

    aws connected successfully

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
-const { Pool } = require("pg");
+require('dotenv').config();
+
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
+
+pool.on('connect', () => console.log('Connected to PostgreSQL'));
 
 module.exports = pool;
